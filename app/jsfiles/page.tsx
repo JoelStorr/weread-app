22e6d0cdc6a5a05b83765c778761aa73116da855
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-09-26 17:40:39 +0200

    Clean up

--- app/jsfiles/page.tsx
+++ app/jsfiles/page.tsx
@@ -1,22 +1,19 @@
-"use client"
-import { invoke } from '@tauri-apps/api';
-import { save } from '@tauri-apps/api/dialog';
-import Link from 'next/link';
-import React, { FormEvent, useRef } from 'react'
+"use client";
+import { invoke } from "@tauri-apps/api";
+import { save } from "@tauri-apps/api/dialog";
+import Link from "next/link";
+import React, { FormEvent, useRef } from "react";
 
 export default function JSFiles() {
-  
+  const textareRef = useRef<HTMLTextAreaElement>(null);
 
-    const textareRef = useRef<HTMLTextAreaElement>(null);
-
-  async function saveCode(e: FormEvent){
-
+  async function saveCode(e: FormEvent) {
     e.preventDefault();
 
-    console.log('Ran save')
+    console.log("Ran save");
 
-    try{
-        const filepath = await save({
+    try {
+      const filepath = await save({
         filters: [
           {
             name: "user",
@@ -29,33 +26,26 @@ export default function JSFiles() {
 
       const replace = textareRef.current?.value.replace(`„`, `"`);
 
-
-      await invoke<void>("save_file", { path: filepath, contents: replace});
-
-
-    } catch (error){
-        console.error(error);
+      await invoke<void>("save_file", { path: filepath, contents: replace });
+    } catch (error) {
+      console.error(error);
     }
-
   }
-  
-  
 
-    return (
+  return (
     <div>
+      <h3>Your JS Code goes here</h3>
 
-        <h3>Your JS Code goes here</h3>
+      <form onSubmit={saveCode}>
+        <textarea ref={textareRef} lang="en" />
+        <br />
+        <button type="submit">Save</button>
+      </form>
+      <br />
+      <button onClick={() => {}}>Load Code</button>
+      <br />
 
-        <form onSubmit={saveCode}>
-            <textarea ref={textareRef} lang='en' />
-            <br/>
-            <button type='submit'>Save</button>
-        </form>
-        <br/>
-        <button onClick={()=>{}}>Load Code</button>
-        <br/>
-        
-        <Link href={"/"}>Home</Link>
+      <Link href={"/"}>Home</Link>
     </div>
-  )
+  );
 }
