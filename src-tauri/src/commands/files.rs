70067b13e3bd0f4e8f37faae41dcee88b86dd971
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-09-27 10:00:10 +0200

    clean up

--- src-tauri/src/commands/files.rs
+++ src-tauri/src/commands/files.rs
@@ -1,33 +1,24 @@
 use std::fs;
 
-    
-    #[tauri::command]
-    pub fn save_file(path: String, contents:String){
-    
-    
-        fs::write(path, contents).unwrap();
-        
-    }
-    
-    
-    #[tauri::command]
-    pub fn load_file(path: String) -> String{
-    
-        let data = fs::read_to_string(path);
+#[tauri::command]
+pub fn save_file(path: String, contents: String) {
+    fs::write(path, contents).unwrap();
+}
 
-        
-        match data {
-            Ok(name) => {
-                return String::from(name);
-            },
-            Err(..) => {
-                println!("Could not load the file")
-            }
-            
+#[tauri::command]
+pub fn load_file(path: String) -> String {
+    let data = fs::read_to_string(path);
+
+    match data {
+        Ok(name) => {
+            return String::from(name);
         }
-        
-        println!("{:#?}", data);
-        
-        return String::from("");
+        Err(..) => {
+            println!("Could not load the file");
+        }
     }
 
+    println!("{:#?}", data);
+
+    return String::from("");
+}
