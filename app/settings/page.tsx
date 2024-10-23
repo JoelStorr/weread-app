3a1f23b45f8eee37fdffcbcae8d87b1ca98636cc
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-10-23 12:02:55 +0200

    Clean up

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,15 +1,11 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export default function SettingsPage() {
   return (
     <div>
-        <h1>
-            Settings Page
-        </h1>
-        <Link href={"/"}>Home</Link>
+      <h1>Settings Page</h1>
+      <Link href={"/"}>Home</Link>
     </div>
-
-
-  )
+  );
 }
